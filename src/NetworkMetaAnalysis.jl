4c2996a77f45aed5b016f464ddcbf201274a36c3
chriselrod <elrodc@gmail.com>
2019-10-04 18:19:37 -0400

    Created library.

--- src/NetworkMetaAnalysis.jl
+++ src/NetworkMetaAnalysis.jl
@@ -1,5 +1,30 @@
 module NetworkMetaAnalysis
 
-greet() = print("Hello World!")
+using SIMDPirates, ReverseDiffExpressions
+
+export RaggedNetworkMetaAnalysis, OffsetNetworkMetaAnalysis, Zero
+
+abstract type AbstractNetworkMetaAnalysis end
+
+"""
+Represents a network as a ragged matrix.
+"""
+struct RaggedNetworkMetaAnalysis <: AbstractNetworkMetaAnalysis
+
+end
+"""
+Represents a network as a series of offsets.
+"""
+struct OffsetNetworkMetaAnalysis <: AbstractNetworkMetaAnalysis
+
+end
+
+"""
+
+"""
+function network_meta_analysis_quote()
+
+
+end
 
 end # module

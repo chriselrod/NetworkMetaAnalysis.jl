e98ce917a42fb81a6f1bdd83f0278c1a1533cd2d
Chris Elrod <elrodc@gmail.com>
2019-10-16 18:25:02 -0400

    Minor updates, but I will create an adjoint-array interface in ReverseDiffExprBase that Emax, ITP, etc, can support to automatically work with many existing distributions.

--- src/functions.jl
+++ src/functions.jl
@@ -1,20 +1,50 @@
 
-struct Eₘₐₓ
+const VectorOrDouble{T} = Union{T,<:AbstractFixedSizeVector{<:Any,T}}
 
+struct Eₘₐₓ{T,N<:AbstractNetwork,M<:VectorOrDouble{T},D<:VectorOrDouble{T},B<:Union{T,Nothing}}
+    N::N
+    Eₘₐₓ::M
+    ED₅₀::D
+    μ::B
 end
+
+@inline Eₘₐₓ(em::M, ed::D) where {M,D} = Eₘₐₓ{M,D,Nothing}(em, ed, nothing)
 
 @generated function ProbabilityDistributions.Normal(
     data::AbstractVector{T},
-    μ::T,
-    emax::Eₘₐₓ,
+    emax::Eₘₐₓ{T,N,M,D,B},
     τ::AbstractVector{T},
     ::Val{track} = Val((false,true,false))
-) where {T,track}
+) where {T,R,N<:GatherNetwork{R},M,D,B,track}
     track_data, track_emax, track_τ = track
     @assert !(track_data | track_τ) # for now; ought to implement later.
 
+    q = quote
+        $(Expr(:meta,:inline)) # we need to inline to avoid allocations from actually constructing Eₘₐₓ object
+        
+    end
     
     
 end
+
+@generated function ProbabilityDistributions.∂Normal(
+    sp::StackPointer,
+    data::AbstractVector{T},
+    emax::Eₘₐₓ{T,N,M,D,B},
+    τ::AbstractVector{T},
+    ::Val{track} = Val((false,true,false))
+) where {T,R,N<:GatherNetwork{R},M,D,B,track}
+
+    track_data, track_emax, track_τ = track
+    @assert !(track_data | track_τ) # for now; ought to implement later.
+
+    q = quote
+        $(Expr(:meta,:inline)) # we need to inline to avoid allocations from actually constructing Eₘₐₓ object
+        
+    end
+    
+
+end
+
 
 
